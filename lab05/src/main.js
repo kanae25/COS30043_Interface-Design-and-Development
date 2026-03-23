fcd8fa3309da5888401aca58141d414cdd284032
datschef <barrycao2005@gmail.com>
2026-03-23 23:29:29 +1100

    reset

--- lab05/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "../assets/js/lib/vue.esm-browser.prod.js";
-import App from "./components/App.js";
-import router from "./router/index.js";
-
-const app = createApp(App);
-app.use(router);
-app.mount("#app");
